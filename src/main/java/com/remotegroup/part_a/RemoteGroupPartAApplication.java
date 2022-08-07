dffd10df196ef2c6a21d7e130bfcd99dc8420803
tjbdev <beornjade@gmail.com>
2022-08-07 16:52:50 +1000

    initialization

---
+++ src/main/java/com/remotegroup/part_a/RemoteGroupPartAApplication.java
@@ -0,0 +1,13 @@
+package com.remotegroup.part_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteGroupPartAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteGroupPartAApplication.class, args);
+	}
+
+}
