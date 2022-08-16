34f3a9b557840816b383feb41ee49c2d033dc9d3
Bodhi <bodhijohnyates@gmail.com>
2022-08-16 18:15:26 +1000

    Update Supplier.java
    updated supplier.java

--- src/main/java/com/remotegroup/procurement/Supplier.java
+++ src/main/java/com/remotegroup/procurement/Supplier.java
@@ -1,5 +1,42 @@
 package com.remotegroup.procurement;
 
-public class Supplier {
+import java.util.Objects;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Supplier {
+	private @Id @GeneratedValue Long id;
+	String companyName;
+	String base;
+	
+	Supplier(String i, String cN, String b){
+		id = i;
+		companyName = cN;
+		base = b;
+	}
+	
+	public String getCompanyName() {
+		return companyName;
+	}
+	
+	public String getBase() {
+		return base;
+	}
+	
+	public String getSupplierId() {
+		return id;
+	}
+	
+	public void setCompanyName(String newName) {
+		this.companyName = newName;
+	}
+	
+	public void setBase(String b) {
+		this.base = b;
+	}
+	
+	public void setId(string newId) {
+		this.id = newId;
+	}
 }
