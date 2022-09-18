5051bd55e5d93bdaac2e083e4a98f8048dda1d62
Andrest07 <57931603+Andrest07@users.noreply.github.com>
2022-09-19 06:44:57 +1000

    Kafka Dependency and imports required

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,22 @@
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.converter.BatchMessagingMessageConverter;
+import org.springframework.kafka.support.converter.JsonMessageConverter;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
 
 @SpringBootApplication
 public class Application {
