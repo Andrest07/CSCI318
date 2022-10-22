def7ec45f32e47a6a77be05fd6a2cd5331a5dd7a
Stellar <yoshidino2000@gmail.com>
2022-10-22 18:29:26 +1100

    Creation of Business Intelligence
    Spring boot is 2.7.5 as 2.7.2 is unavailable to me

---
+++ Business-Intelligence/businessintellegence/src/main/java/com/remotegroup/businessintellegence/BusinessintellegenceApplication.java
@@ -0,0 +1,13 @@
+package com.remotegroup.businessintellegence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessintellegenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessintellegenceApplication.class, args);
+	}
+
+}
---
+++ Business-Intelligence/businessintellegence/src/test/java/com/remotegroup/businessintellegence/BusinessintellegenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remotegroup.businessintellegence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessintellegenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
